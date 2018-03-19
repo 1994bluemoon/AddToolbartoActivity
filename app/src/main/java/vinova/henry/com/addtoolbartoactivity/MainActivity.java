f9ecf49afbd0b2f09c633ba222accbd396d443e0
Henry Duong <minhhoang94_bluemoon@yahoo.com.vn>
2018-03-19 22:06:02 +0700

    first

--- app/src/main/java/vinova/henry/com/addtoolbartoactivity/MainActivity.java
+++ app/src/main/java/vinova/henry/com/addtoolbartoactivity/MainActivity.java
@@ -1,7 +1,9 @@
 package vinova.henry.com.addtoolbartoactivity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(myToolbar);
+
+        ActionBar ab = getSupportActionBar();
     }
 }
